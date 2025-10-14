271f7ca1357cd794462904c697b740eaffb61752
D4viMartinsLima <martinslimafkc@gmail.com>
2025-10-14 18:26:42 -0300

    Picking random words from CSV file

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+#----------------------------------------------------------------------------#
+#Acessar os dados e gerar um DataFrame
+data = pandas.read_csv("data/1000_palavras_espanhol_portugues.csv")
+#Usar DataFrame.to_dict(orient="records")
+to_learn = data.to_dict(orient="records")
+
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text = "Espanhol")
+    canvas.itemconfig(card_word, text = current_card["Es"])
+
+
+
+
+# #-----------------------------------UI-----------------------------------------#
 
 window = Tk()
 window.title("Flash Card App")
@@ -9,17 +26,19 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400,263, image = card_front_img)
-canvas.create_text(400, 150,text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400,263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150,text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400,263, text="", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR)
 canvas.grid(column=0,row=0, columnspan=2)
 
 x_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=x_image, highlightthickness=0)
+unknown_button = Button(image=x_image, highlightthickness=0, command=next_card)
 unknown_button.grid(column=0, row=1)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0)
+known_button = Button(image=check_image, highlightthickness=0,command=next_card)
 known_button.grid(column=1, row=1)
+
+next_card()
 
 window.mainloop()
